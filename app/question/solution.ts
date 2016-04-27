5f2705b01e144c5aff855d44bfd53be704248d42
James Salas <j@jamesbs.com>
2016-04-26 23:44:38 -0400

    add method for comparing provided answers to answer keys, add shim and existing typings to system

--- app/question/solution.ts
+++ app/question/solution.ts
@@ -1,9 +1,31 @@
 import { Injectable } from 'angular2/core';
+import { Question } from './question';
 import { QuestionType } from './question-type';
 
 
 @Injectable()
 export class Solution {
-    type: QuestionType;
+    type: QuestionType; // necessary?
     value: string;
+}
+
+
+export const normalize = (str: string): string => {
+    return str.toLowerCase().trim();
+}
+
+export const validateSolution = (solution: Solution, question: Question): boolean => {
+    if(question.type === 'english') {
+        const transform = normalize;
+        const comparison: (a: string, b: string | string[]) => boolean =
+              (typeof question.langItem.english === 'string')
+            ? (provided: string, answer: string): boolean => provided === transform(answer)
+            : (provided: string, answer: string[]) => answer.map(transform).indexOf(provided) !== -1;
+            
+       return comparison(normalize(solution.value), question.langItem.english);
+    } else if(question.type === 'pinyin') {
+        const transform = (a: string) => normalize(a).replace(' ', '');
+        
+        return transform(solution.value) === transform(question.langItem.pinyin);
+    }
 }
--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/definitions/core/core.d.ts" />
