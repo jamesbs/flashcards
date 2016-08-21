23a1ccad5bc6472fba6611101b639e5e3c8c4df9
James Salas <j@jamesbs.com>
2016-08-21 16:36:07 -0400

    do some styling for new card

--- src/lang-item/lang-item.ts
+++ src/lang-item/lang-item.ts
@@ -12,8 +12,29 @@ export class LangItem implements Translation {
 }
     
 export const getCharacters = (langItem: LangItem): Character[] =>
-  zip(langItem.chinese.split(''), langItem.pinyin.split(' '))
+  zip(langItem.chinese.split(''), splitPinyin(langItem.pinyin))
     .map(([ chinese, pinyin ]: [ string, string ]): Character => ({
       chinese,
       pinyin: fromBasic(pinyin)
     }));
+
+export const splitPinyin = (pinyin: string) => {
+  let index = 0;
+  let pinyins: string[] = [];
+  let current = '';
+
+  pinyin.split(/(\d)/)
+    .filter(token => token !== '')
+    .map(token => token.trim())
+    .forEach(token => {
+      if(index === 0) {
+        current = token;
+        index = 1;
+      } else {
+        pinyins.push(current + token);
+        index = 0;
+      }
+    })
+
+  return pinyins;
+}
