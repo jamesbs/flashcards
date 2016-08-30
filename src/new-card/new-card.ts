7b78760eab37e7fd91af7110543a365b6a6a47db
James Salas <j@jamesbs.com>
2016-08-30 17:13:21 -0400

    new card unused ref cleanup

--- src/new-card/new-card.ts
+++ src/new-card/new-card.ts
@@ -1,14 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core'
-import { LangItemService } from '../lang-item/lang-item.service'
-import { LangItem, SimpleTranslation, Translation, Character, Word } from '../domain/models'
-import { getCharacters, getWords } from '../domain/lang-item'
-import { toStandard } from '../domain/pinyin'
-
-const cardFormat = (characters: Character[]) =>
-  characters.map(({ chinese, pinyin }) => ({
-    chinese,
-    pinyin: toStandard(pinyin)
-  }))
+import { LangItem } from '../domain/models'
+import { getCharacters } from '../domain/lang-item'
 
 @Component({
   selector: 'app-new-card',
@@ -22,6 +14,4 @@ export class NewCard {
   get characters() {
     return getCharacters(this.langItem)
   }
-
-  constructor(private langItemService: LangItemService) { }
 }
