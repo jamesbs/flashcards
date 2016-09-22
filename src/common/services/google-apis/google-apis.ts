2d5e404f7b5df449790ecd2854eec92786a2f668
James Salas <j@jamesbs.com>
2016-09-22 17:52:31 -0400

    fix issue with google auth not firing properly

--- src/common/services/google-apis/google-apis.ts
+++ src/common/services/google-apis/google-apis.ts
@@ -1,37 +1,78 @@
 import { Injectable, Inject } from '@angular/core'
 import { googleApisClientId } from './google-apis-client-id'
+import { AsyncSubject, Subject, BehaviorSubject, Observable, Subscriber } from 'rxjs'
+
+const execAsync = <T>(run: (subject: AsyncSubject<T>) => void) => {
+  const subject = new AsyncSubject<T>()
+  run(subject)
+  return subject
+}
+
+const lazyAsync = <T>(run: (subject: AsyncSubject<T>) => void) => {
+  const sub = new AsyncSubject<T>()
+  let fired = false
+
+  return Observable.of(sub)
+    .flatMap(s => {
+      if (!fired) {
+        run(s)
+        fired = true
+      }
+
+      return s
+    })
+}
 
 @Injectable()
 export class GoogleApis {
   loginUsingGoogle() {
     const scopes = 'profile'
 
-    return new Promise((resolve, reject) => {
-        // console.log('resolve', resolve, 'reject', reject)
-        if(!gapi.auth2) {
-          gapi.load('auth2', () => {
-            // console.log('auth2 done')
-            resolve()
-          })
-        } else {
-          // console.log('has auth2')
-          resolve()
-        }
-      })
-      .then(() => {
-        // console.log('initializing auth2', this.clientId, scopes)
-        return gapi.auth2.init({
-          client_id: this.clientId,
-          scopes
-        })
-      })
-      .then(() => {
-        // console.log('attempting to sign in')
-        return gapi.auth2.getAuthInstance().signIn()
+    this.auth2Init
+      .subscribe(user => {
+        console.log('user', user)
       })
   }
 
+  loadClient = execAsync(subject => {
+    gapi.load('client', () => {
+      subject.next(undefined)
+      subject.complete()
+    })
+  })
+
+  loadAuth2 = lazyAsync(subject => {
+      gapi.load('auth2', () => {
+        subject.next(undefined)
+        subject.complete()
+      })
+    })
+
+  auth2Init = lazyAsync(subject => {
+      gapi.auth2.init(this.auth2Config)
+        .then((...args) => {
+          subject.next(args)
+          subject.complete()
+        })
+    })
+
+  auth2Config = { client_id: this.clientId, scopes: 'profile' }
+
+  getAuthInstance = lazyAsync(subject => {
+    console.log('auth2 val', gapi.auth2)
+    gapi.auth2.getAuthInstance()
+      .isSignedIn.listen(res => {
+        console.log('signed in!')
+        subject.next(res)
+        subject.complete()
+      })
+  })
+
   constructor(@Inject(googleApisClientId) private clientId: string) {
-    gapi.load('client', () => undefined)
+    const loadClientSubscription = this.loadClient
+      .mergeMap(() => this.loadAuth2)
+      .subscribe(() => {
+        loadClientSubscription.unsubscribe()
+      })
   }
 }
